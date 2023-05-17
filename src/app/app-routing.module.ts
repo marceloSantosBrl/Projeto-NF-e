3325014e85f03e3ef943e95b1385352f94a0f516
marcelo <marcelegald2@outlook.com>
2023-05-17 12:41:26 -0300

    Criação das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', loadComponent: () =>
+      import('src/app/pages/home/home.component')
+        .then(m => m.HomeComponent)},
+  {path: 'estoque-vendas', loadComponent: () =>
+      import('src/app/pages/inventory-reversals/inventory-reversals.component')
+        .then(m => m.InventoryReversalsComponent)},
+  {path: 'estoque-total', loadComponent: () =>
+      import('src/app/pages/inventory-total/inventory-total.component')
+        .then(m => m.InventoryTotalComponent)},
+  {path: 'estoque-estornos', loadComponent: () =>
+      import('src/app/pages/inventory-reversals/inventory-reversals.component')
+        .then(m => m.InventoryReversalsComponent)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
